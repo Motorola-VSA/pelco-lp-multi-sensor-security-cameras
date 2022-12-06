d32ca313e191741a913da9a6ade2584664f90adf
Lee <lee.keller@motorolasolutions.com>
2022-12-06 12:15:53 -0500

    multi

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       xl: '1440px',
     },
     backgroundImage: {
-      'marquee-desktop': "url('https://3396890.fs1.hubspotusercontent-na1.net/hubfs/3396890/Pelco/Landing%20Pages/multi-sensor-security-cameras/12.%20Panoramic%20Multi%20Sensor%20Desktop.jpg')",
+      'marquee-desktop': "url('https://3396890.fs1.hubspotusercontent-na1.net/hubfs/3396890/Pelco/Landing%20Pages/multi-sensor-security-cameras/12.%20Panoramic%20Multi%20Sensor%20Desktop%20(1).jpg')",
       'marquee-mobile': "url('https://3396890.fs1.hubspotusercontent-na1.net/hubfs/3396890/Pelco/Landing%20Pages/multi-sensor-security-cameras/12.%20Panoramic%20Multi%20Sensor%20Mobile.jpg')"
     },
     extend: {
